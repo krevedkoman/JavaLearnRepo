35e053e788ab95773c15913ae9184cded8b0dfbf
ilya mak <ilya@makashov.kz>
2020-06-02 17:00:42 -0700

    start

---
+++ src/main/java/com/krevedkoman/javacore/chapter2/BlockTest.java
@@ -0,0 +1,14 @@
+package main.java.com.krevedkoman.javacore.chapter2;
+
+public class BlockTest {
+    public static void main(String[] args) {
+        int x, y;
+        y = 20;
+
+        for (x = 0; x < 10; x++){
+            System.out.println("Значение x: " + x);
+            System.out.println("Значение y: " + y);
+            y = y - 2;
+        }
+    }
+}
