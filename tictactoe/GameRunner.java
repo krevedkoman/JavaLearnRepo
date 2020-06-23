bab0b2b68ebb8787c6c922a239b0bcec8b8c1cb5
ilya mak <ilya@makashov.kz>
2020-06-23 10:58:54 -0700

    commit

---
+++ tictactoe/GameRunner.java
@@ -0,0 +1,8 @@
+package main.java.com.krevedkoman.javacore.tictactoe;
+
+public class GameRunner {
+    public static void main(String[] args) {
+    GameLogic gl = new GameLogic();
+    gl.start();
+    }
+}
